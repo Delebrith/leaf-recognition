d9fb7fc7165f706a1e7ad75d15d9d8ea9ca57b38
p.szwed <p.szwed@samsung.com>
2018-11-27 12:31:23 +0100

    Convert images into tfrecords

--- src/prepare_datasets.py
+++ src/prepare_datasets.py
@@ -1,12 +1,13 @@
 import random
 import os
-import math
+import datetime
+import sys
 
 import tensorflow as tf
 import cv2
 import numpy as np
 
-#===============DEFINE ARGUMENTS==============
+# ===============DEFINE ARGUMENTS==============
 flags = tf.app.flags
 
 flags.DEFINE_string('data_src', './../data', 'String: Your dataset directory')
@@ -16,12 +17,10 @@ flags.DEFINE_float('validation_set_size', 0.2,
 
 flags.DEFINE_float('test_set_size', 0.2, 'Float: The proportion of examples in the dataset to be used for testing')
 
-flags.DEFINE_integer('shards', 1, 'Int: Number of shards to split the TFRecord files')
-
 # Seed for repeatability.
 flags.DEFINE_integer('random_seed', 0, 'Int: Random seed to use for repeatability.')
 
-flags.DEFINE_string('tfrecord_file', 'data.tfrecord', 'String: The output filename to name your TFRecord file')
+flags.DEFINE_string('tfrecord_file', 'data.tfrecord', 'String: The base of output filename to name your TFRecord file')
 
 FLAGS = flags.FLAGS
 
@@ -35,26 +34,29 @@ def main():
 
     image_files, classes = _get_files_in_classes(FLAGS.data_src)
     class_ids_dict = dict(zip(classes, range(len(classes))))
+    print('Total images: %d' % len(image_files))
 
     validation_index = int(FLAGS.validation_set_size * len(image_files))
-    test_index = validation_index + int(FLAGS.test_set_size * len(image_files))
+    test_index = int(FLAGS.test_set_size * len(image_files)) + validation_index
 
     random.seed(FLAGS.random_seed)
     random.shuffle(image_files)
-    training_files = image_files[validation_index:]
-    validation_files = image_files[validation_index:test_index]
-    test_files = image_files[:test_index]
+    validation_files = image_files[0:validation_index]
+    test_files = image_files[validation_index:test_index]
+    training_files = image_files[test_index:]
 
-    _prepare_tfrecord('training', training_files, class_ids_dict, FLAGS.shards, FLAGS.data_src, FLAGS.tfrecord_file)
-    _prepare_tfrecord('test', test_files, class_ids_dict, FLAGS.shards, FLAGS.data_src, FLAGS.tfrecord_file)
-    _prepare_tfrecord('validation', validation_files, class_ids_dict, FLAGS.shards, FLAGS.data_src, FLAGS.tfrecord_file)
+    timestamp = datetime.datetime.now().isoformat()
+    _prepare_tfrecord('training', training_files, class_ids_dict, FLAGS.data_src, FLAGS.tfrecord_file, timestamp)
+    _prepare_tfrecord('test', test_files, class_ids_dict, FLAGS.data_src, FLAGS.tfrecord_file, timestamp)
+    _prepare_tfrecord('validation', validation_files, class_ids_dict, FLAGS.data_src, FLAGS.tfrecord_file, timestamp)
 
 
 def _get_files_in_classes(data_src):
+    class_directory = os.listdir(data_src)[0]
     directories = []
     classes = []
-    for element in os.listdir(data_src):
-        path = os.path.join(data_src, element)
+    for element in os.listdir(os.path.join(data_src, class_directory)):
+        path = os.path.join(os.path.join(data_src, class_directory), element)
         if os.path.isdir(path):
             directories.append(path)
             classes.append(element)
@@ -72,19 +74,6 @@ def _get_files_in_classes(data_src):
     return images, classes
 
 
-def _prepare_tfrecord(type, images, class_dict, shards, data_src, tfrecord_filename):
-    assert type in ['training', 'validation', 'test']
-    images_per_shard = int(math.ceil(len(images)/float(shards)))
-
-    filename = os.path.join(data_src, type + '.' + tfrecord_filename)
-
-    writer = tf.python_io.TFRecordWriter(filename)
-
-
-
-    return 0
-
-
 def _read_image(path):
     image = cv2.imread(path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -93,11 +82,34 @@ def _read_image(path):
 
 
 def _int64_feature(value):
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
 def _bytes_feature(value):
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _prepare_tfrecord(type, files, class_dict, data_src, tfrecord_filename, timestamp):
+    assert type in ['training', 'validation', 'test']
+
+    filename = os.path.join(data_src, '%s-%s-%s.tfrecord' % (
+        type, tfrecord_filename, timestamp
+    ))
+    writer = tf.python_io.TFRecordWriter(filename)
+
+    for i in range(0, len(files)):
+        print('Including image %d/%d into %s dataset' % (i+1, len(files), type))
+        sys.stdout.flush()
+
+        image = _read_image(files[i])
+        classname = os.path.basename(os.path.dirname(files[i]))
+        feature = {'class_id': _int64_feature(class_dict[classname]),
+                   'data': _bytes_feature(tf.compat.as_bytes(np.array2string(image)))}
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+        writer.write(example.SerializeToString())
+
+    writer.close()
 
 
 if __name__ == "__main__":
