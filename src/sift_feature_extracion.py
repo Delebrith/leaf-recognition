3d493fba459bad0ec13000df5978fcfe206ffa7d
p.szwed <p.szwed@samsung.com>
2018-11-28 16:24:21 +0100

    Change options to filter only features in grayscale or hsv

--- src/sift_feature_extracion.py
+++ src/sift_feature_extracion.py
@@ -12,13 +12,10 @@ flags.DEFINE_string('data_src', './../data_src', 'String: Your dataset directory
 
 flags.DEFINE_string('data_dest', './../data_filtered', 'String: Directory where results of filtration will be stored')
 
-flags.DEFINE_string('features_only', 'no', 'String: "yes" or "no", if filtering result should overlap original image'
-                                           'or be drawn on black bitmap')
+flags.DEFINE_string('colors', 'gray', 'String: Filter image in rgb->grayscale or rgb->hsv')
 
 flags.DEFINE_string('rich_keypoints', 'no', 'String: "yes" or "no", if filtering result should be flagged as '
                                             'DRAW_RICH_KEYPOINTS')
-
-flags.DEFINE_string('gray_scale', 'no', 'String: "yes" or "no", if source image in output should be in grayscale')
 
 FLAGS = flags.FLAGS
 
@@ -37,8 +34,8 @@ def main():
     if not os.path.exists(FLAGS.data_dest):
         os.makedirs(FLAGS.data_dest)
 
-    if FLAGS.features_only not in ['yes', 'no'] or FLAGS.rich_keypoints not in ['yes', 'no']:
-        raise ValueError('invalid arguments in --features_only or --rich_keypoints')
+    if FLAGS.colors not in ['gray', 'hsv'] or FLAGS.rich_keypoints not in ['yes', 'no']:
+        raise ValueError('invalid arguments in --colors or --rich_keypoints')
 
     images, classes = get_files_in_classes(FLAGS.data_src)
     sift_extraction(images, classes, FLAGS.data_dest)
@@ -56,31 +53,22 @@ def sift_extraction(images, classes, dest_dir):
 
         image_path = images[i]
         image = cv2.imread(image_path)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        if FLAGS.colors == 'gray':
+            processed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        else:
+            processed = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
+
         sift = cv2.xfeatures2d.SIFT_create()
-        kp = sift.detect(gray, None)
+        kp = sift.detect(processed, None)
 
         blank_image = np.zeros((image.shape[0], image.shape[1], 3), np.uint8)
 
-        if FLAGS.features_only == 'yes':
-            if FLAGS.rich_keypoints == 'yes':
-                features = cv2.drawKeypoints(blank_image, kp, blank_image,
-                                             flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            else:
-                features = cv2.drawKeypoints(blank_image, kp, blank_image, flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+        if FLAGS.rich_keypoints == 'yes':
+            features = cv2.drawKeypoints(blank_image, kp, blank_image,
+                                         flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         else:
-            if FLAGS.rich_keypoints == 'yes':
-                if FLAGS.gray_scale == 'yes':
-                    features = cv2.drawKeypoints(gray, kp, blank_image,
-                                                 flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                else:
-                    features = cv2.drawKeypoints(image, kp, blank_image,
-                                                 flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            else:
-                if FLAGS.gray_scale == 'yes':
-                    features = cv2.drawKeypoints(gray, kp, blank_image, flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
-                else:
-                    features = cv2.drawKeypoints(image, kp, blank_image, flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+            features = cv2.drawKeypoints(blank_image, kp, blank_image, flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
 
         classname = os.path.split(os.path.split(image_path)[0])[1]
         features_path = os.path.join(os.path.join(inner_dir, classname), os.path.basename(image_path))
