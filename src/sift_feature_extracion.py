a07dc13847de722d98bd823dac3a08d3dc8d806f
Chmielok <chmielok@gmail.com>
2018-12-16 17:24:18 +0100

    Saving features to CSV file

--- src/sift_feature_extracion.py
+++ src/sift_feature_extracion.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 from src.file_utils import get_files_in_classes
 
 # ===============DEFINE ARGUMENTS==============
@@ -62,19 +63,23 @@ def sift_extraction(images, classes, dest_dir):
             processed = cv2.cvtColor(image, cv2.COLOR_BGR2HSV_FULL)
 
         sift = cv2.xfeatures2d.SIFT_create()
-        kp = sift.detect(processed, None)
+        kps = sift.detect(processed)
+        decoded_kps = np.asarray([(kp.response, kp.pt[0], kp.pt[1], kp.angle, kp.size, kp.octave) for kp in kps])
+        kps_as_df = pd.DataFrame(decoded_kps, columns=["response", "x_pos", "y_pos", "angle", "size", "octave"])
 
         blank_image = np.zeros((image.shape[0], image.shape[1], 3), np.uint8)
 
         if FLAGS.rich_keypoints == 'yes':
-            features = cv2.drawKeypoints(blank_image, kp, blank_image,
+            features = cv2.drawKeypoints(blank_image, kps, blank_image,
                                          flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         else:
-            features = cv2.drawKeypoints(blank_image, kp, blank_image, flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
+            features = cv2.drawKeypoints(blank_image, kps, blank_image, flags=cv2.DRAW_MATCHES_FLAGS_DEFAULT)
 
         classname = os.path.split(os.path.split(image_path)[0])[1]
         features_path = os.path.join(os.path.join(inner_dir, classname), os.path.basename(image_path))
         cv2.imwrite(features_path, features)
+        csv_path = features_path.split(".")[0] + ".csv"
+        kps_as_df.to_csv(csv_path)
 
 
 if __name__ == "__main__":
